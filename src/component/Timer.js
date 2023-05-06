15bcbc02a38931e4e37895430060df3ae429a9e4
Deepak Verma <sardiwaldeepak.145@gmail.com>
2023-05-06 20:58:15 +0530

    Added all Main Files

---
+++ src/component/Timer.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+const Timer = ({onReqTimerStart}) => {
+   const [timeInSec, setTimeInSec] = useState('');
+  return (
+    <div>
+        <label htmlFor='time-count'>Enter Time in seconds</label>
+        <input type='number' id='time-count' placeholder='Enter Time in secs'
+        value={timeInSec}
+        onChange={(e) => setTimeInSec(e.target.value)}
+        onKeyDown={(e)=>{
+            // console.log(e)
+            if (e.keyCode === 13 || e.which === 13) {
+              onReqTimerStart && onReqTimerStart(Math.floor(timeInSec));
+              setTimeInSec('');
+            }
+
+        }}
+        />
+    </div>
+  )
+}
+
+export default Timer
